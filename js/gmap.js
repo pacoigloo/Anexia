e4e68811d022959420324064ace7babc69101bb4
pacoigloo <felipe.zapata@igloo-lab.com.co>
2015-04-14 01:19:26 +0000

    Version 1.0.1

--- js/gmap.js
+++ js/gmap.js
@@ -1,60 +1,69 @@
-
-
-
 function initialize(lugar) {
     var centerUn = new google.maps.LatLng(-34.608367,-58.372283);
+    var titledo, zoomit;
     
-   /* if(lugar == 'sitio')
-    {
-        centerUn = new google.maps.LatLng(-34.608367,-58.372283);
-    }
-    else
-    {
          if(lugar == 'plaza')
         {
              centerUn = new google.maps.LatLng(-34.608367,-58.372283);
+            titledo = 'Plaza de mayo';
+            zoomit = 17;
         }
          else
         {
             if(lugar == 'telmo')
             {
-                centerUn = new google.maps.LatLng(-34.626753,-58.3726185);
+                centerUn = new google.maps.LatLng(-34.619990,-58.370966);
+                titledo = 'San Telmo';
+                zoomit = 12;
             }
             else
             {
                if(lugar == 'boca')
                 {
                     centerUn = new google.maps.LatLng(-34.634852,-58.361373);
+                    titledo = 'La Boca';
+                    zoomit = 12;
                 }
                 else
                 {
                     if(lugar == 'palermo')
                     {
                         centerUn = new google.maps.LatLng(-34.5888895,-58.4305555);
+                         titledo = 'Palermo';
+                         zoomit = 12;
                     }
                     else
                     {
                         if(lugar == 'recoleta')
                         {
                             centerUn = new google.maps.LatLng(-34.5862763,-58.3877598);
+                             titledo = 'Recoleta';
+                             zoomit = 12;
                         }
                         else
                         {
                             if(lugar == 'plata')
                             {
-                                centerUn = new google.maps.LatLng(-35.1394419,-56.7406742);
+                                centerUn = new google.maps.LatLng(-34.754926,-58.039053);
+                                 titledo = 'Rio la Plata';
+                                 zoomit = 9;
                             }
                         } 
                     } 
                 } 
             }
         }
-    }*/
+    
       var mapProp = {
         center:centerUn,
-        zoom:18,
+        zoom:zoomit,
         mapTypeId:google.maps.MapTypeId.ROADMAP
       };
-      var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+      
+      var map=new google.maps.Map(document.getElementById("map"), mapProp);
+      var marker2 = new google.maps.Marker({
+        position: map.getCenter()
+        , title:titledo
+        , map: map
+        , });
     }
-    google.maps.event.addDomListener(window, 'load', initialize);
--- js/route.js
+++ js/route.js
@@ -10,6 +10,7 @@ app.config(function($routeProvider){
           })
           .when('/sitios',{
                 templateUrl: 'sitios.html'
+                
           })
           .when('/conferencista',{
                 templateUrl: 'conferencista.html'
@@ -22,8 +23,19 @@ app.config(function($routeProvider){
           });
 });
 app.controller('cfgController',function($scope){
-    
+
 });
+
+app.controller('mapa', function ($scope) {
+    var mapOptions = {
+        zoom: 10,
+        center: new google.maps.LatLng(-34.594905,-58.390226),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+});
+
 
 $( "#menucito" ).click(function() {
     $( "#menu-flotante" ).animate({ "left": "0" }, "slow" );
@@ -75,22 +87,88 @@ function imgMedicoNet(valor){
     $(".net-medico").css("height",miAlto);
     $(".net-foto").css("height",miAlto);
 }
+
 //Funcion para mostrar horarios de la agenda.
 function MostrarHorarios(dia){
     //
+    var f = new Date();
+    //document.write(f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear());
+    var btn1 = document.getElementById("btn1");
+    var btn2 = document.getElementById("btn2");
+    var btn3 = document.getElementById("btn3");
+    var btn4 = document.getElementById("btn4");
+    var btn5 = document.getElementById("btn5");
+    
     var dia1 = document.getElementById("dia1");
     var dia2 = document.getElementById("dia2");
     var dia3 = document.getElementById("dia3");
     var dia4 = document.getElementById("dia4");
     var dia5 = document.getElementById("dia5");
+    
     dia2.style.display="none";
     dia3.style.display="none";
     dia4.style.display="none";
     dia5.style.display="none";
+    
+    switch (f.getDate()){
+        case 29:
+            // code
+            btn1.style.backgroundColor="#2FA1F1";
+            btn1.style.color="white";
+            btn2.style.backgroundColor="#3F3F3F";
+            btn3.style.backgroundColor="#3F3F3F";
+            btn4.style.backgroundColor="#3F3F3F";
+            btn5.style.backgroundColor="#3F3F3F";
+            break;
+        case 30:
+            // code
+            btn1.style.backgroundColor="#3F3F3F";
+            btn2.style.backgroundColor="#2FA1F1";
+            btn1.style.color="white";
+            btn3.style.backgroundColor="#3F3F3F";
+            btn4.style.backgroundColor="#3F3F3F";
+            btn5.style.backgroundColor="#3F3F3F";
+            break;
+        case 1:
+            // code
+            btn1.style.backgroundColor="#3F3F3F";
+            btn2.style.backgroundColor="#3F3F3F";
+            btn3.style.backgroundColor="#2FA1F1";
+            btn1.style.color="white";
+            btn4.style.backgroundColor="#3F3F3F";
+            btn5.style.backgroundColor="#3F3F3F";
+            break;
+        case 2:
+            // code
+            btn1.style.backgroundColor="#3F3F3F";
+            btn2.style.backgroundColor="#3F3F3F";
+            btn3.style.backgroundColor="#3F3F3F";
+            btn4.style.backgroundColor="#2FA1F1";
+            btn1.style.color="white";
+            btn5.style.backgroundColor="#3F3F3F";
+            break;
+        case 3:
+            // code
+            btn1.style.backgroundColor="#3F3F3F";
+            btn2.style.backgroundColor="#3F3F3F";
+            btn3.style.backgroundColor="#3F3F3F";
+            btn4.style.backgroundColor="#3F3F3F";
+            btn1.style.color="white";
+            btn5.style.backgroundColor="#2FA1F1";
+            break;
+        default:
+            // code
+    }
     //Verificamos que boton estan presionado y mostramos la vista.
-    switch (dia) {
+    switch (dia){
         case '1':
             // code
+            btn1.style.backgroundColor="#2FA1F1";
+            btn2.style.backgroundColor="#3F3F3F";
+            btn3.style.backgroundColor="#3F3F3F";
+            btn4.style.backgroundColor="#3F3F3F";
+            btn5.style.backgroundColor="#3F3F3F";
+            
             dia1.style.display="block";
             dia2.style.display="none";
             dia3.style.display="none";
@@ -99,6 +177,12 @@ function MostrarHorarios(dia){
             break;
         case '2':
             // code
+            btn1.style.backgroundColor="#3F3F3F";
+            btn2.style.backgroundColor="#2FA1F1";
+            btn3.style.backgroundColor="#3F3F3F";
+            btn4.style.backgroundColor="#3F3F3F";
+            btn5.style.backgroundColor="#3F3F3F";
+            
             dia1.style.display="none";
             dia2.style.display="block";
             dia3.style.display="none";
@@ -107,6 +191,12 @@ function MostrarHorarios(dia){
             break;
         case '3':
             // code
+            btn1.style.backgroundColor="#3F3F3F";
+            btn2.style.backgroundColor="#3F3F3F";
+            btn3.style.backgroundColor="#2FA1F1";
+            btn4.style.backgroundColor="#3F3F3F";
+            btn5.style.backgroundColor="#3F3F3F";
+            
             dia1.style.display="none";
             dia2.style.display="none";
             dia3.style.display="block";
@@ -115,6 +205,12 @@ function MostrarHorarios(dia){
             break;
         case '4':
             // code
+            btn1.style.backgroundColor="#3F3F3F";
+            btn2.style.backgroundColor="#3F3F3F";
+            btn3.style.backgroundColor="#3F3F3F";
+            btn4.style.backgroundColor="#2FA1F1";
+            btn5.style.backgroundColor="#3F3F3F";
+            
             dia1.style.display="none";
             dia2.style.display="none";
             dia3.style.display="none";
@@ -123,6 +219,12 @@ function MostrarHorarios(dia){
             break;
         case '5':
             // code
+            btn1.style.backgroundColor="#3F3F3F";
+            btn2.style.backgroundColor="#3F3F3F";
+            btn3.style.backgroundColor="#3F3F3F";
+            btn4.style.backgroundColor="#3F3F3F";
+            btn5.style.backgroundColor="#2FA1F1";
+            
             dia1.style.display="none";
             dia2.style.display="none";
             dia3.style.display="none";
